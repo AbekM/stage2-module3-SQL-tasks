9e39e9147a065096deac5f25b6f00b919b0aa297
AbekM <asilbek.makhmudov@outlook.com>
2023-10-10 17:49:29 +0500

    l9 donzo

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -10,7 +10,7 @@ CREATE TABLE Subject
     id          BIGINT AUTO_INCREMENT PRIMARY KEY,
     name        VARCHAR,
     description VARCHAR,
-    grade       INT CHECK (grade >=1 AND grade <=5)
+    grade       INT CHECK (grade >= 1 AND grade <= 5)
 );
 CREATE TABLE PaymentType
 (
@@ -18,10 +18,10 @@ CREATE TABLE PaymentType
     name VARCHAR UNIQUE
 );
 
-CREATE TABLE Payments
+CREATE TABLE Payment
 (
     id           BIGINT AUTO_INCREMENT PRIMARY KEY,
-    type_id      BIGINT NOT NULL ,
+    type_id      BIGINT NOT NULL,
     amount       DECIMAL,
     student_id   BIGINT,
     payment_date DATETIME,
@@ -31,9 +31,9 @@ CREATE TABLE Payments
 CREATE TABLE Mark
 (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    student_id BIGINT NOT NULL ,
-    subject_id BIGINT NOT NULL ,
-    mark       INT CHECK (mark >=1 AND mark <=10),
+    student_id BIGINT NOT NULL,
+    subject_id BIGINT NOT NULL,
+    mark       INT CHECK (mark >= 1 AND mark <= 10),
     FOREIGN KEY (student_id) REFERENCES Student (id),
     FOREIGN KEY (subject_id) REFERENCES Subject (id)
 );
@@ -97,15 +97,15 @@ VALUES ('WEEKLY');
 INSERT INTO PaymentType(name)
 VALUES ('MONTHLY');
 //Payments
-INSERT INTO Payments(type_id, student_id)
+INSERT INTO Payment(type_id, student_id)
 VALUES (2, 1);
-INSERT INTO Payments(type_id, student_id)
+INSERT INTO Payment(type_id, student_id)
 VALUES (3, 4);
-INSERT INTO Payments(type_id, student_id)
+INSERT INTO Payment(type_id, student_id)
 VALUES (2, 7);
-INSERT INTO Payments(type_id, student_id)
+INSERT INTO Payment(type_id, student_id)
 VALUES (1, 5);
-INSERT INTO Payments(type_id, student_id)
+INSERT INTO Payment(type_id, student_id)
 VALUES (1, 12);
 //Marks
 INSERT INTO Mark(student_id, subject_id, mark)
@@ -132,7 +132,108 @@ INSERT INTO Mark(student_id, subject_id, mark)
 VALUES ((SELECT id FROM Student WHERE name = 'Elena'),
         (SELECT id FROM Subject WHERE name = 'IT'),
         1);
+//task4
+SELECT *
+FROM Student;
+SELECT *
+FROM Student
+LIMIT 50;
+SELECT name
+FROM Student;
+SELECT DISTINCT amount
+FROM Payment;
+//task5
+SELECT *
+FROM Payment
+WHERE amount >= 500;
+SELECT *
+FROM Student
+WHERE DATEADD('YEAR', 20, birthday) < CURRENT_DATE;
+SELECT *
+FROM Student
+WHERE "group" = 10
+  AND DATEADD('YEAR', 20, birthday) > CURRENT_DATE;
+SELECT *
+FROM Student
+WHERE name = 'Mike'
+   OR "group" IN (4, 5, 6);
+SELECT *
+FROM Payment
+WHERE DATEADD('MONTH', -8, CURRENT_DATE) <= payment_date;
+SELECT *
+FROM Student
+WHERE name LIKE 'A%';
+SELECT *
+FROM Student
+WHERE (name = 'Roxi' AND "group" = 6)
+   OR (name = 'Tallie' AND "group" = 9);
+//task6
+SELECT *
+FROM Payment
+WHERE type_id = (SELECT id FROM PaymentType WHERE name = 'MONTHLY');
+SELECT *
+FROM Mark
+WHERE subject_id = (SELECT id FROM Subject WHERE name = 'Art');
+SELECT *
+FROM Student
+         JOIN Payment P on Student.id = P.student_id
+WHERE P.type_id = (SELECT id FROM PaymentType WHERE name = 'WEEKLY');
+SELECT *
+FROM Student
+         JOIN Mark M on Student.id = M.student_id AND subject_id = (SELECT id FROM Subject WHERE Subject.name = 'Math');
+//task7
+SELECT *
+FROM Mark
+WHERE mark > 6
+ORDER BY mark DESC;
+SELECT *
+FROM Payment
+WHERE amount < 300
+ORDER BY amount;
+SELECT *
+FROM PaymentType
+ORDER BY name;
+SELECT *
+FROM Student
+ORDER BY name DESC;
+SELECT *
+FROM Student
+         JOIN Payment P on Student.id = P.student_id
+WHERE p.amount > 1000
+ORDER BY birthday;
+//task8
+SELECT MIN(birthday)
+FROM Student;
+SELECT MIN(payment_date)
+FROM Payment;
+SELECT AVG(mark)
+FROM Mark
+WHERE subject_id = (SELECT id FROM Subject WHERE name = 'Math');
+SELECT MIN(amount)
+FROM Payment
+         JOIN PaymentType PT on PT.id = Payment.type_id
+WHERE pt.name = 'WEEKLY';
+//task9
+INSERT INTO Payment(type_id, amount, student_id, payment_date)
+VALUES (1, 2, 4, '2019-04-05');
+SELECT *
+FROM Student s
+         JOIN (SELECT student_id, AVG(mark) as average_mark FROM Mark GROUP BY student_id HAVING AVG(mark) > 8)
+              ON s.id = student_id;
+SELECT DISTINCT id, name
+FROM Student s
+         JOIN (SELECT student_id, MIN(mark) as minimum_mark FROM Mark GROUP BY student_id HAVING MIN(mark) > 7)
+              ON s.id = student_id;
+SELECT id, name
+FROM Student s
+         JOIN (SELECT student_id, COUNT(*) as payment_count
+               FROM Payment
+               WHERE YEAR(payment_date) = 2019
+               GROUP BY student_id
 
--- SELECT * from Subject where name='';
-
-
+               HAVING COUNT(*) > 2) ON s.id = student_id;
+SELECT student_id, COUNT(*) as payment_count
+FROM Payment
+WHERE YEAR(payment_date) = 2019
+GROUP BY student_id
+HAVING COUNT(*) > 2
